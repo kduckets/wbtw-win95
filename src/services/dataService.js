12d16332da52928472b1f933c562981722e39e16
kduckets <kmditroia@gmail.com>
2021-09-18 13:32:52 -0400

    demos

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -9,10 +9,7 @@ export default class DataService {
         icon: 'info_bubble',
         content: {
           paragraphs: [
-            "Hi! I am a Software Developer based in Toronto. For the last several years I've been working as a Full Stack developer in big FinTech companies.",
-            "I believe that friendly and respectful communication within a team is key to the company's success. With my experience in software development and database design, I think I would be a valuable asset to your project.",
-            'The main technology I have been using is C# .NET. But a few years ago I fell in love with Node.js and I use it for most of my side projects. Moreover, I am in a good relationship with React (this website runs on React btw) and fascinated by Big Data and Blockchain.',
-            'When I am not in front of the computer, I usually do yoga, drink a specialty coffee and go on hikes (lake Yoho in Alberta is my favorite trail so far).',
+            
           ],
         },
       }
--- src/utils/startWebamp.js
+++ src/utils/startWebamp.js
@@ -6,11 +6,105 @@ const startWebamp = () => {
             initialTracks: [{
                 metaData: {
                     artist: "we broke the weather",
-                    title: "the fog (demo)"
+                    title: "Bellwether (demo)"
+                },
+                url: `${process.env.PUBLIC_URL}/Bellwether_Demo.mp3`,
+                duration: 213
+            },
+            {
+                metaData: {
+                    artist: "we broke the weather",
+                    title: "In Web (demo)"
+                },
+                url: `${process.env.PUBLIC_URL}/In Web_Demo.mp3`,
+                duration: 213
+            },
+
+            {
+                metaData: {
+                    artist: "we broke the weather",
+                    title: "Onyx Pooling (demo)"
+                },
+                url: `${process.env.PUBLIC_URL}/Onyx Pooling_demo.mp3`,
+                duration: 213
+            },
+
+            {
+                metaData: {
+                    artist: "we broke the weather",
+                    title: "Niceberg (demo)"
+                },
+                url: `${process.env.PUBLIC_URL}/Niceberg_demo.mp3`,
+                duration: 213
+            },
+
+            {
+                metaData: {
+                    artist: "we broke the weather",
+                    title: "Boneloader (demo)"
+                },
+                url: `${process.env.PUBLIC_URL}/Boneloader_demo.mp3`,
+                duration: 213
+            },
+
+            {
+                metaData: {
+                    artist: "we broke the weather",
+                    title: "Fire Season (demo)"
+                },
+                url: `${process.env.PUBLIC_URL}/Fire Season_demo.mp3`,
+                duration: 213
+            },
+
+            {
+                metaData: {
+                    artist: "we broke the weather",
+                    title: "Through the Wall (demo)"
+                },
+                url: `${process.env.PUBLIC_URL}/Through The Wall_demo.mp3`,
+                duration: 213
+            },
+
+            {
+                metaData: {
+                    artist: "we broke the weather",
+                    title: "Rot King (demo)"
+                },
+                url: `${process.env.PUBLIC_URL}/Rot King_demo.mp3`,
+                duration: 213
+            },
+
+            {
+                metaData: {
+                    artist: "we broke the weather",
+                    title: "Frequency (demo)"
+                },
+                url: `${process.env.PUBLIC_URL}/Frequency_demo.mp3`,
+                duration: 213
+            },
+
+            {
+                metaData: {
+                    artist: "we broke the weather",
+                    title: "These Old Bones (demo)"
+                },
+                url: `${process.env.PUBLIC_URL}/These Old Bones_demo.mp3`,
+                duration: 213
+            },
+
+            
+
+            {
+                metaData: {
+                    artist: "we broke the weather",
+                    title: "The Fog (demo)"
                 },
                 url: `${process.env.PUBLIC_URL}/The Fog_demo.mp3`,
                 duration: 213
-            }]
+            },
+        
+        
+        ]
         });
         webamp.onClose(() => {
             webamp.dispose();
