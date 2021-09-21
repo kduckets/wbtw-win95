da132210fc338aa6673f93ac13defb913637b9d5
kduckets <kmditroia@gmail.com>
2021-09-20 22:15:19 -0400

    Update About.js

--- src/components/NotepadContent/About.js
+++ src/components/NotepadContent/About.js
@@ -42,13 +42,16 @@ function About({ content }) {
 
                    <p>nick - keys, sax, flute, etc, vocals</p>
                   
-                   <p>andy - drums, vocals, count-offs</p>
+                   <p>andy - drums, vocals, count-ins</p>
                  
                    <p>steve - bass, moog, brown notes</p>
                      
                    <p>scott - guitar, sax, vocals, garage banding</p>
 
                    <p>kev - guitar/loud noises</p></i>
+
+                   <br />
+         
 
                    <p><a className="email" href="mailto:webroketheweather@gmail.com">webroketheweather@gmail.com</a></p>
                  
