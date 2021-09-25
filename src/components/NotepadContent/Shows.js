7db3fa48515ff8a11dcfb61168cbbd1808539d44
kduckets <kmditroia@gmail.com>
2021-09-25 13:33:44 -0400

    Update Shows.js

--- src/components/NotepadContent/Shows.js
+++ src/components/NotepadContent/Shows.js
@@ -21,11 +21,7 @@ function Shows({ content }) {
           
             <div>
                 <br/>
-        
-            <a href="https://membership.onlineaction.org/site/Donation2?df_id=19284&19284.donation=form1&addl_info=nav-button&conversion_pg=www.edf.org%2F&_ga=2.192113130.1549475860.1607045714-634390749.1607045714" 
-              target="_blank" rel="noopener noreferrer">
-              <button><small>fix the weather</small></button>
-              </a>
+       
               <br/>
               <br/>
               </div>  
