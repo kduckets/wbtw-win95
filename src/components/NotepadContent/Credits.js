f267f081232bb5c131c3cfeef9057e42bb7b5471
kduckets <kmditroia@gmail.com>
2022-01-14 10:29:43 -0500

    Update Credits.js

--- src/components/NotepadContent/Credits.js
+++ src/components/NotepadContent/Credits.js
@@ -15,7 +15,7 @@ function Credits({ content }) {
 
                    <p>jspaint - <a href='https://github.com/1j01/jspaint' target='_blank' rel="noopener noreferrer">https://github.com/1j01/jspaint</a></p>
                  
-                   <p>minesweeper - <a href='https://github.com/oL-web/react-minesweeper' target='_blank' rel="noopener noreferrer">https://github.com/oL-web/react-minesweeper</a></p>
+                   <p>mines - <a href='https://github.com/oL-web/react-minesweeper' target='_blank' rel="noopener noreferrer">https://github.com/oL-web/react-minesweeper</a></p>
                      
                    <p>synth - <a href='https://github.com/kevinsqi/react-piano' target='_blank' rel="noopener noreferrer">https://github.com/kevinsqi/react-piano</a></p>
 
