de28fb69ad773d8a1c3521febb5780d2f952de8b
kduckets <kmditroia@gmail.com>
2022-01-18 06:53:39 -0500

    Update Merch.js

--- src/components/NotepadContent/Merch.js
+++ src/components/NotepadContent/Merch.js
@@ -8,12 +8,15 @@ function Merch({ content }) {
     return (
         <div>
             <h2>wbtw merch table</h2>  
-
+    <hr></hr>
             <h3>eco wallet compact disc</h3>  
             <p>CD comes in 100% plastic-free eco wallet gatefold</p>
             <a href="https://webroketheweather.bandcamp.com" target="_blank" rel="noopener noreferrer">
-            <img src='/cd.jpg' alt='wbtw-cover' width='80%' ></img>
+            <img src='/cd.jpg' alt='wbtw-cover' width='90%' ></img>
             </a>
+          <br/>
+          <br/>
+            <hr></hr>
       
           <h3>t-shirts</h3>
           <p>cool t-shirts coming soon</p>  
