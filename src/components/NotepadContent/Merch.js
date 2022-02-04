d7e607c82aa9909ec2d29112ee039f5f867b3025
kduckets <kmditroia@gmail.com>
2022-02-04 11:13:10 -0500

    t-shirt

--- src/components/NotepadContent/Merch.js
+++ src/components/NotepadContent/Merch.js
@@ -12,19 +12,25 @@ function Merch({ content }) {
             <h3>eco wallet compact disc</h3>  
             <p>CD comes in 100% plastic-free eco wallet gatefold</p>
            
-            <a href="https://webroketheweather.bandcamp.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://webroketheweather.bandcamp.com/album/we-broke-the-weather" target="_blank" rel="noopener noreferrer">
             <img src='/cd.jpg' alt='wbtw-cd' width='90%' ></img>
             </a>
             <h4>$10</h4>
             <p>
-            <a href="https://webroketheweather.bandcamp.com" target="_blank" rel="noopener noreferrer"><button>pre-order on bandcamp</button></a>
+            <a href="https://webroketheweather.bandcamp.com/album/we-broke-the-weather" target="_blank" rel="noopener noreferrer"><button>pre-order on bandcamp</button></a>
             </p>
           <br/>
             <hr></hr>
       
           <h3>t-shirts</h3>
-          <p>cool t-shirts coming soon</p>  
-          <img src='/t-shirt.jpg' alt='wbtw-tshirt' width='80%' ></img>
+          <p>Wish your friends said “Hey, cool tshirt” more often? Try this one.</p>  
+          <a href="https://webroketheweather.bandcamp.com/merch/rot-king-t-shirt" target="_blank" rel="noopener noreferrer">
+            <img src='/t-shirt.jpg' alt='wbtw-cd' width='90%' ></img>
+            </a>
+            <h4>$20</h4>
+            <p>
+            <a href="https://webroketheweather.bandcamp.com/merch/rot-king-t-shirt" target="_blank" rel="noopener noreferrer"><button>pre-order on bandcamp</button></a>
+            </p>
           <br/>
           <br/>
             <hr></hr>
