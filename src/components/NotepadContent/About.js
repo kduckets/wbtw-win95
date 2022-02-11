53c139aa2e194f4639b274610ff6fc9738f0e057
kduckets <kmditroia@gmail.com>
2022-02-10 22:08:30 -0500

    Update About.js

--- src/components/NotepadContent/About.js
+++ src/components/NotepadContent/About.js
@@ -36,14 +36,14 @@ function About({ content }) {
 
     return (
         <div>
-            <h2>we broke the weather</h2>
+           
 
             {/* <h4>debut album out now!</h4> */}
             <a href="https://webroketheweather.bandcamp.com?campaign=website" target="_blank" rel="noopener noreferrer">
-            <img src='/wbtw-cover.jpg' alt='wbtw-cover' width='60%' ></img>
+            <img src='/wbtw-cover.jpg' alt='wbtw-cover' width='100%' ></img>
             </a> 
-            <iframe title="wbtw-streams" width="60%" height="52" src="https://odesli.co/embed/?url=https%3A%2F%2Falbum.link%2F3bzzbq2pft9qh&theme=light" frameborder="0" allowfullscreen sandbox="allow-same-origin allow-scripts allow-presentation allow-popups allow-popups-to-escape-sandbox" allow="clipboard-read; clipboard-write"></iframe>
- 
+            <iframe title="wbtw-streams" width="100%" height="52" src="https://odesli.co/embed/?url=https%3A%2F%2Falbum.link%2F3bzzbq2pft9qh&theme=light" frameborder="0" allowfullscreen sandbox="allow-same-origin allow-scripts allow-presentation allow-popups allow-popups-to-escape-sandbox" allow="clipboard-read; clipboard-write"></iframe>
+            <h2>we broke the weather</h2>
             <small>
            
                  <p>wbtw is:</p> 
