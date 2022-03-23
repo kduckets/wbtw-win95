0f8ac0af3109bdc54cafc03155c87dbb42c87550
kduckets <kmditroia@gmail.com>
2022-03-23 10:48:03 -0400

    Update Shows.js

--- src/components/NotepadContent/Shows.js
+++ src/components/NotepadContent/Shows.js
@@ -12,8 +12,12 @@ function Shows({ content }) {
             <small>
          
             <p><b>4.14.22 </b> &nbsp;Ralph's Rock Diner - Worcester, MA <a href='https://fb.me/e/4v1erneLT' target='_blank' rel="noopener noreferrer"> (more info)</a></p>
+            <p><i>with T & the Wrecks, Bass Sabbath, and Allison & Moon</i></p>
             <p><b>5.14.22 </b> &nbsp;Porchfest - Somerville, MA <a href='https://somervilleartscouncil.org/porchfest' target='_blank' rel="noopener noreferrer"> (more info)</a></p>
+            <p><b>5.31.22 </b> &nbsp;O'Brien's Pub - Allston, MA <a href='https://obrienspubboston.com/' target='_blank' rel="noopener noreferrer"> (more info)</a></p>
+            <p><i>with Bad Larrys and Me in Capris</i></p>
 
+            <br/>
             <p><i>more spring '22 show dates coming soon</i></p>
      
                  
