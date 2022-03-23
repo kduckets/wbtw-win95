9aecb5f47e02e33dba9c93a1bc98a7d926744543
kduckets <kmditroia@gmail.com>
2022-03-22 20:21:08 -0400

    Update Shows.js

--- src/components/NotepadContent/Shows.js
+++ src/components/NotepadContent/Shows.js
@@ -11,8 +11,8 @@ function Shows({ content }) {
             <h3>live, in concert!</h3>
             <small>
          
-            <p><b>4.14.22 </b> &nbsp;Ralph's Rock Diner - Worcester, MA  <a href='https://fb.me/e/4v1erneLT' target='_blank' rel="noopener noreferrer"> &nbsp;(more info)</a></p>
-            <p><b>5.14.22 </b> &nbsp;Porchfest - Somerville, MA  <a href='https://somervilleartscouncil.org/porchfest' target='_blank' rel="noopener noreferrer"> &nbsp;(more info)</a></p>
+            <p><b>4.14.22 </b> &nbsp;Ralph's Rock Diner - Worcester, MA <a href='https://fb.me/e/4v1erneLT' target='_blank' rel="noopener noreferrer"> (more info)</a></p>
+            <p><b>5.14.22 </b> &nbsp;Porchfest - Somerville, MA <a href='https://somervilleartscouncil.org/porchfest' target='_blank' rel="noopener noreferrer"> (more info)</a></p>
 
             <p><i>more spring '22 show dates coming soon</i></p>
      
