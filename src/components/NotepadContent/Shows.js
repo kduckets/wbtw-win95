913b2bb79245cfa777d73922fd252ca6bbf720b4
kduckets <kmditroia@gmail.com>
2022-03-24 17:04:08 -0400

    show posters

--- src/components/NotepadContent/Shows.js
+++ src/components/NotepadContent/Shows.js
@@ -15,15 +15,19 @@ function Shows({ content }) {
             <p><b>4.14.22 // Worcester, MA</b> <a href='https://www.facebook.com/events/1021644978564043/' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>Ralph's Rock Diner <br/>
             <small><i>Bass Sabbath, T & the Wrecks, and Allison & Moon</i></small></p>
             <p><b>4.21.22 // Somerville, MA</b> <a href='https://www.facebook.com/events/639047397192438' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>The Jungle <br/>
-            <small><i>Blacktop Strut, The Love Shamans, and Desperate Electric</i></small></p>
+            <small><i>The Love Shamans and Desperate Electric</i></small></p>
             <p><b>5.14.22 // Somerville, MA</b> <a href='https://somervilleartscouncil.org/node/48180' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>Porchfest <br/>
            </p>
-            <p><b>5.31.22 // Allston, MA</b><a href='https://obrienspubboston.com/' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a> <br/>O'Brien's Pub<br/>
+            <p><b>5.31.22 // Allston, MA</b><a href='https://fb.me/e/1o5ayh2Ty' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a> <br/>O'Brien's Pub<br/>
             <small><i>Bad Larrys and Me in Capris</i></small></p>
             <br/>
             {/* <a href='https://www.facebook.com/events/1021644978564043/' target='_blank' rel="noopener noreferrer">
-            <img alt='wbtw-raplhs' src='/ralphs-poster.jpg' style={{width:'50%'}}/>
+            <img alt='wbtw-ralphs' src='/ralphs-poster.jpg' style={{width:'50%'}}/>
+            </a>
+            <a href='https://fb.me/e/1o5ayh2Ty' target='_blank' rel="noopener noreferrer">
+            <img alt='wbtw-obriens' src='/obriens-poster.png' style={{width:'50%'}}/>
             </a> */}
+
             <img alt='wbtw-live' src='/wbtw-midway.jpg' style={{width:'100%'}}/>
 
      
