c1c809081305f99b6ca92676eacf3f9007938043
kduckets <kmditroia@gmail.com>
2022-03-24 08:13:26 -0400

    Update Shows.js

--- src/components/NotepadContent/Shows.js
+++ src/components/NotepadContent/Shows.js
@@ -10,13 +10,13 @@ function Shows({ content }) {
             <h2>we broke the weather</h2>
             <h3>live, in concert!</h3>
 
-            <p><b>4.14.22 // Worcester, MA</b> &nbsp;Ralph's Rock Diner <a href='https://fb.me/e/4v1erneLT' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>
+            <p><b>4.14.22 // Worcester, MA</b> <a href='https://fb.me/e/4v1erneLT' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>Ralph's Rock Diner <br/>
             <small><i>Bass Sabbath, T & the Wrecks, and Allison & Moon</i></small></p>
-            <p><b>4.21.22 // Somerville, MA</b> &nbsp;The Jungle <a href='https://www.facebook.com/events/639047397192438' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>
+            <p><b>4.21.22 // Somerville, MA</b> <a href='https://www.facebook.com/events/639047397192438' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>The Jungle <br/>
             <small><i>Blacktop Strut, The Love Shamans, and Desperate Electric</i></small></p>
-            <p><b>5.14.22 // Somerville, MA</b> &nbsp;Porchfest <a href='https://somervilleartscouncil.org/node/48180' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>
+            <p><b>5.14.22 // Somerville, MA</b> <a href='https://somervilleartscouncil.org/node/48180' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>Porchfest <br/>
            </p>
-            <p><b>5.31.22 // Allston, MA</b> &nbsp;O'Brien's Pub<a href='https://obrienspubboston.com/' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>
+            <p><b>5.31.22 // Allston, MA</b><a href='https://obrienspubboston.com/' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a> <br/>O'Brien's Pub<br/>
             <small><i>Bad Larrys and Me in Capris</i></small></p>
             <br/>
             <img alt='wbtw-live' src='https://webroketheweather.com/wbtw-midway.jpg' style={{width:'100%'}}/>
