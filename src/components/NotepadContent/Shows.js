49b3ddb34bf1fbd8771903e49b6b27ed0d985d88
kduckets <kmditroia@gmail.com>
2022-03-24 08:09:45 -0400

    Update Shows.js

--- src/components/NotepadContent/Shows.js
+++ src/components/NotepadContent/Shows.js
@@ -10,14 +10,14 @@ function Shows({ content }) {
             <h2>we broke the weather</h2>
             <h3>live, in concert!</h3>
 
-            <p><b>4.14.22 </b> &nbsp;Ralph's Rock Diner - Worcester, MA <a href='https://fb.me/e/4v1erneLT' target='_blank' rel="noopener noreferrer"> <small>(info)</small></a><br/>
-            <small><i>w/ Bass Sabbath, T & the Wrecks, and Allison & Moon</i></small></p>
-            <p><b>4.21.22 </b> &nbsp;The Jungle - Somerville, MA <a href='https://www.facebook.com/events/639047397192438' target='_blank' rel="noopener noreferrer"> <small>(info)</small></a><br/>
-            <small><i>w/ Blacktop Strut, The Love Shamans, and Desperate Electric</i></small></p>
-            <p><b>5.14.22 </b> &nbsp;Porchfest - Somerville, MA <a href='https://somervilleartscouncil.org/node/48180' target='_blank' rel="noopener noreferrer"> <small>(info)</small></a><br/>
+            <p><b>4.14.22 // Worcester, MA</b> &nbsp;Ralph's Rock Diner <a href='https://fb.me/e/4v1erneLT' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>
+            <small><i>Bass Sabbath, T & the Wrecks, and Allison & Moon</i></small></p>
+            <p><b>4.21.22 // Somerville, MA</b> &nbsp;The Jungle <a href='https://www.facebook.com/events/639047397192438' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>
+            <small><i>Blacktop Strut, The Love Shamans, and Desperate Electric</i></small></p>
+            <p><b>5.14.22 // Somerville, MA</b> &nbsp;Porchfest <a href='https://somervilleartscouncil.org/node/48180' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>
            </p>
-            <p><b>5.31.22 </b> &nbsp;O'Brien's Pub - Allston, MA <a href='https://obrienspubboston.com/' target='_blank' rel="noopener noreferrer"> <small>(info)</small></a><br/>
-            <small><i>w/ Bad Larrys and Me in Capris</i></small></p>
+            <p><b>5.31.22 // Allston, MA</b> &nbsp;O'Brien's Pub<a href='https://obrienspubboston.com/' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>
+            <small><i>Bad Larrys and Me in Capris</i></small></p>
             <br/>
             <img alt='wbtw-live' src='https://webroketheweather.com/wbtw-midway.jpg' style={{width:'100%'}}/>
 
