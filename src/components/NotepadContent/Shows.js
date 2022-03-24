d401b67603e03439984ee1f14ae1f49f17ecc4d7
kduckets <kmditroia@gmail.com>
2022-03-24 07:48:47 -0400

    Update Shows.js

--- src/components/NotepadContent/Shows.js
+++ src/components/NotepadContent/Shows.js
@@ -15,7 +15,7 @@ function Shows({ content }) {
             <p><b>4.21.22 </b> &nbsp;The Jungle - Somerville, MA <a href='https://www.facebook.com/events/639047397192438' target='_blank' rel="noopener noreferrer"> <small>(info)</small></a><br/>
             <small><i>w/ Blacktop Strut, The Love Shamans, and Desperate Electric</i></small></p>
             <p><b>5.14.22 </b> &nbsp;Porchfest - Somerville, MA <a href='https://somervilleartscouncil.org/node/48180' target='_blank' rel="noopener noreferrer"> <small>(info)</small></a><br/>
-            <small><i>w/ every band in the Somerville vicinity</i></small></p>
+           </p>
             <p><b>5.31.22 </b> &nbsp;O'Brien's Pub - Allston, MA <a href='https://obrienspubboston.com/' target='_blank' rel="noopener noreferrer"> <small>(info)</small></a><br/>
             <small><i>w/ Bad Larrys and Me in Capris</i></small></p>
             <br/>
