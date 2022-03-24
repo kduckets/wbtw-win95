42116dd5cbb295fe9a1427c2577251d34cd9695d
kduckets <kmditroia@gmail.com>
2022-03-24 12:50:59 -0400

    Update Merch.js

--- src/components/NotepadContent/Merch.js
+++ src/components/NotepadContent/Merch.js
@@ -8,7 +8,7 @@ function Merch({ content }) {
     return (
         <div>
             <h2>wbtw merch table</h2>  
-    <hr></hr>
+            <hr class="rounded"></hr> 
             <h3>eco wallet compact disc</h3>  
             <p>CD comes in 100% plastic-free eco wallet gatefold</p>
            
@@ -20,7 +20,7 @@ function Merch({ content }) {
             <a href="https://webroketheweather.bandcamp.com/album/we-broke-the-weather?campaign=website" target="_blank" rel="noopener noreferrer"><button>order on bandcamp</button></a>
             </p>
           <br/>
-            <hr></hr>
+          <hr class="rounded"></hr> 
       
           <h3>t-shirts</h3>
           <p>wish your friends said “hey, cool tshirt” more often? try this one.</p>  
@@ -33,7 +33,7 @@ function Merch({ content }) {
             </p>
           <br/>
           <br/>
-            <hr></hr>
+          <hr class="rounded"></hr> 
             <h3>sticker pack</h3>
           <p>grab some stickers for your stickerless surfaces</p>  
           <a href="https://webroketheweather.bandcamp.com/merch/wbtw-vinyl-sticker-pack?campaign=website" target="_blank" rel="noopener noreferrer">
@@ -45,7 +45,7 @@ function Merch({ content }) {
             </p>
           <br/>
           <br/>
-            <hr></hr>
+          <hr class="rounded"></hr> 
             <br/>
        
        <br/>
