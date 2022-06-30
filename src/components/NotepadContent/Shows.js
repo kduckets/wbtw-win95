d0e7af28ad2934c97b332d66f441bed4124f9c91
kduckets <kmditroia@gmail.com>
2022-06-29 20:43:50 -0400

    Update Shows.js

--- src/components/NotepadContent/Shows.js
+++ src/components/NotepadContent/Shows.js
@@ -66,62 +66,49 @@ function Shows({ content }) {
                 }
                 </StyledList>
                 </StyledDiv>
+                <hr class="rounded"></hr> 
+
+            <h3>upcoming shows</h3>
+            
             <hr class="rounded"></hr> 
 
-            <h3>live, in concert!</h3>
+            <h3>past shows</h3>
 
-            <p><b>3.30.22 // Cambridge, MA</b> <a href='https://www.mideastoffers.com/event/11958255/who-iam-we-broke-the-weather-as-seen-from-space' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>Middle East Upstairs <br/>
+            <p><b>3.30.22 - Cambridge, MA</b> <br/>Middle East Upstairs <br/>
             <small><i>As Seen From Space, Who.iAm</i></small></p>
-            <p><b>4.14.22 // Worcester, MA</b> <a href='https://www.facebook.com/events/1021644978564043/' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>Ralph's Rock Diner <br/>
+            <a href='https://www.mideastoffers.com/event/11958255/who-iam-we-broke-the-weather-as-seen-from-space' target='_blank' rel="noopener noreferrer">
+            <img alt='wbtw-mideast' src='/mideast-poster.png' style={{width:'50%'}}/>
+            </a>
+            <p><b>4.14.22 - Worcester, MA</b><br/>Ralph's Rock Diner <br/>
             <small><i>Bass Sabbath, T & the Wrecks, Allison & Moon</i></small></p>
-            <p><s style={{color:'red'}}><b>4.21.22 // Somerville, MA</b> <br/>The Jungle <br/>
+            <a href='https://www.facebook.com/events/1021644978564043/' target='_blank' rel="noopener noreferrer">
+            <img alt='wbtw-ralphs' src='/ralphs-poster.jpg' style={{width:'50%'}}/>
+            </a>
+            <p><s style={{color:'red'}}><b>4.21.22 - Somerville, MA</b> <br/>The Jungle <br/>
            </s></p>
-            <p><s style={{color:'red'}}><b>4.27.22 // Providence, RI</b> <br/>Dusk <br/>
+            <p><s style={{color:'red'}}><b>4.27.22 - Providence, RI</b> <br/>Dusk <br/>
            </s></p>
-            <p><b>5.14.22 // Somerville, MA</b> <a href='https://somervilleartscouncil.org/node/48180' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>Porchfest <br/>
+           <a href='https://www.facebook.com/events/939692190071596/' target='_blank' rel="noopener noreferrer">
+            <img alt='wbtw-dusk' src='/wbtw-dusk-poster.jpg' style={{width:'50%'}}/>
+            </a>
+            <p><b>5.14.22 - Somerville, MA</b><br/>Porchfest <br/>
            </p>
-            <p><b>5.31.22 // Allston, MA</b><a href='https://www.facebook.com/events/1123949811738104/' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a> <br/>O'Brien's Pub<br/>
+           <a href='https://somervilleartscouncil.org/node/48180' target='_blank' rel="noopener noreferrer">
+            <img alt='wbtw-porchfest' src='/porchfest-poster.jpg' style={{width:'50%'}}/>
+            </a>
+            <p><b>5.31.22 - Allston, MA</b><br/>O'Brien's Pub<br/>
             <small><i>Bad Larrys, Me in Capris</i></small></p>
-            <p> <b>6.21.22 // Somerville, MA</b> <a href='https://www.facebook.com/events/1692579974410214/' target='_blank' rel="noopener noreferrer"><small>  (info)</small></a><br/>Out of the Blue Too at the Somerville Armory <br/>
+            <a href='https://www.facebook.com/events/1123949811738104/' target='_blank' rel="noopener noreferrer">
+            <img alt='wbtw-obriens' src='/obriens-poster.png' style={{width:'50%'}}/>
+            </a>
+            <p> <b>6.21.22 - Somerville, MA</b> <br/>Out of the Blue Too at the Somerville Armory <br/>
             <small><i>Varona, The Rupert Selection, Parma Chai</i></small></p>
+            <a href='https://www.facebook.com/events/1692579974410214/' target='_blank' rel="noopener noreferrer">
+            <img alt='wbtw-parma chai' src='/armory-poster.jpg' style={{width:'50%'}}/>
+            </a>
          
             <br/>
-            <hr class="rounded"></hr> 
-            <br/>
-            <a href='https://www.mideastoffers.com/event/11958255/who-iam-we-broke-the-weather-as-seen-from-space' target='_blank' rel="noopener noreferrer">
-            <img alt='wbtw-mideast' src='/mideast-poster.png' style={{width:'100%'}}/>
-            </a>
-            <br/>
-            <br/>
-            <a href='https://www.facebook.com/events/1021644978564043/' target='_blank' rel="noopener noreferrer">
-            <img alt='wbtw-ralphs' src='/ralphs-poster.jpg' style={{width:'100%'}}/>
-            </a>
-            <br/>
-            <br/>
-            <a href='https://www.facebook.com/events/939692190071596/' target='_blank' rel="noopener noreferrer">
-            <img alt='wbtw-dusk' src='/wbtw-dusk-poster.jpg' style={{width:'100%'}}/>
-            </a>
-            <br/>
-            <br/>
-            <a href='https://somervilleartscouncil.org/node/48180' target='_blank' rel="noopener noreferrer">
-            <img alt='wbtw-porchfest' src='/porchfest-poster.jpg' style={{width:'100%'}}/>
-            </a>
-            <br/>
-            <br/>
-            <a href='https://www.facebook.com/events/1123949811738104/' target='_blank' rel="noopener noreferrer">
-            <img alt='wbtw-obriens' src='/obriens-poster.png' style={{width:'100%'}}/>
-            </a>
-            <br/>
-            <br/>
-            <a href='https://www.facebook.com/events/1692579974410214/' target='_blank' rel="noopener noreferrer">
-            <img alt='wbtw-parma chai' src='/armory-poster.jpg' style={{width:'100%'}}/>
-            </a>
 
-     
-                 
-           
-        
-          
             <div>
                 <br/>
        
