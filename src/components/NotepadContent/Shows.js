a32a1a9f4d2952ab248db955c6bb8c3db9092df4
kduckets <kmditroia@gmail.com>
2022-10-03 19:41:11 -0400

    show

--- src/components/NotepadContent/Shows.js
+++ src/components/NotepadContent/Shows.js
@@ -73,7 +73,7 @@ function Shows({ content }) {
 
             <p><b>10.23.22 - Allston, MA</b> <a href='https://www.eventbrite.com/e/blk-jks-we-broke-the-weather-obriens-tickets-396699969287' target='_blank' rel="noopener noreferrer"> <FaInfoCircle/></a><br/>
             <i>O'Brien's Pub</i><br/>
-            <small><a href='https://www.feepband.com' target='_blank' rel="noopener noreferrer">Feep</a>, <a href='https://www.instagram.com/bikethrasher_/' target='_blank' rel="noopener noreferrer">Bike Thrasher</a></small></p>
+            <small><a href='https://www.feepband.com' target='_blank' rel="noopener noreferrer">FEEP</a>, <a href='https://www.instagram.com/bikethrasher_/' target='_blank' rel="noopener noreferrer">Bike Thrasher</a></small></p>
 
             <hr class="rounded"></hr> 
 
