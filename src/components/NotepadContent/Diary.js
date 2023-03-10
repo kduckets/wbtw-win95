b7cb6fc5b5a28ca22d7dc62eb2b5231ac7c7b48c
kduckets <kmditroia@gmail.com>
2023-03-10 05:43:37 -0500

    Update Diary.js

--- src/components/NotepadContent/Diary.js
+++ src/components/NotepadContent/Diary.js
@@ -6,12 +6,10 @@ function Diary({ content }) {
     return (
         <div>
             <h4>kev's diary - keep out, dude!</h4>
-            <p><small>A band</small></p> 
         
            <p>
             <small>
-            make a band. when a band gets loud, one listens. if a band get soft, one may not. a band drowns, in fear! a band is free            </small>   </p>
-<p><small>love, kev</small></p>
+           [redacted]          </small>   </p>
               <br/>
               <br/>
 
