51aae7e9614e0c2c731c85059e5b3693257fb055
kduckets <kmditroia@gmail.com>
2023-03-09 22:06:11 -0500

    Update Diary.js

--- src/components/NotepadContent/Diary.js
+++ src/components/NotepadContent/Diary.js
@@ -10,8 +10,8 @@ function Diary({ content }) {
         
            <p>
             <small>
-            make a band.  a band is a unit. a band is a farce. a force. a single entity of multiple individuals. once one has a band, A band, keep a band. a band has shame. a band has power. more power than any one. a band must not have one leader. a band must fight. a band must agree. no, disagree. a band will suck; hard. a band is nothing, really. a band is real, but not really. a band fakes it. a band makes songs. a band feels. a band is directionless, but the next step is obviously contiguous. if a band is lost, now, the past is impervious. a band hides. a band chills. a band sweats. a band bleeds, when cut. a band seeks. a band feels; good. a band lies. if a band gets loud, one listens. if a band get soft, one may not. a band drowns, in fear. a band is free.            </small>
-          </p>
+            make a band.  a band is a unit. a band is a farce. a force. a single entity of multiple individuals. once one has a band, A band, keep a band. a band has shame. a band has power. more power than any one. a band must not have one leader. a band must fight. a band must agree. no, disagree. a band will suck; hard. a band is nothing, really. a band is real (but not really). a band fakes. a band makes, songs. a band feels. a band is directionless, but the next step is obviously contiguous. if a band is lost, now, the past is impervious. a band hides. a band chills. a band sweats. a band bleeds, when cut. a band seeks. a band feels; good. a band lies. if a band gets loud, one listens. if a band get soft, one may not. a band drowns, in fear. a band is free. 
+            </small>          </p>
               <br/>
               <br/>
 
