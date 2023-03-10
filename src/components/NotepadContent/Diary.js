09af048743b70f73b8b19e5d96696ec5eff1c8ed
kduckets <kmditroia@gmail.com>
2023-03-09 21:43:02 -0500

    Update Diary.js

--- src/components/NotepadContent/Diary.js
+++ src/components/NotepadContent/Diary.js
@@ -8,10 +8,9 @@ function Diary({ content }) {
             <h4>kev's diary - keep out, dude.</h4>
             <h4>A band</h4> 
             <small>
-           
-                   <p>make a band.  a band is a unit. a band is a farce. a force. a single entity of multiple individuals. once one has a band, A band, one must do anything in one’s power to keep a band. a band has power. more power than any one individual. a band must not have one leader. a band must fight. a band must agree. no, disagree. a band will suck, hard. a band is nothing, really. a band is real, but not really. a band fakes it. a band makes songs. a band feels. a band is directionless, but the next step is obviously next. if a band is lost, now, the past is impervious. a band hides. a band chills. a band sweats. a band bleeds, when cut. a band seeks. a band feels; good. a band lies. if a band gets loud, one listens. if a band get soft, one may not. a band drowns, in fear. a band is free. 
-                    </p>               
-           
+           <p>
+            make a band.  a band is a unit. a band is a farce. a force. a single entity of multiple individuals. once one has a band, A band, one must do anything in one’s power to keep a band. a band has power. more power than any one individual. a band must not have one leader. a band must fight. a band must agree. no, disagree. a band will suck, hard. a band is nothing, really. a band is real, but not really. a band fakes it. a band makes songs. a band feels. a band is directionless, but the next step is obviously contiguous. if a band is lost, now, the past is impervious. a band hides. a band chills. a band sweats. a band bleeds, when cut. a band seeks. a band feels; good. a band lies. if a band gets loud, one listens. if a band get soft, one may not. a band drowns, in fear. a band is free.                                
+            </p>    
                   </small>
           
                
