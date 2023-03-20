38e1f9ab7ab0f2ce0a2341bebd28cbaa7951e746
kduckets <kmditroia@gmail.com>
2023-03-20 13:53:06 -0400

    show

--- src/components/NotepadContent/Shows.js
+++ src/components/NotepadContent/Shows.js
@@ -69,7 +69,7 @@ function Shows({ content }) {
                 <hr class="rounded"></hr> 
 
             <h3>upcoming shows</h3>
-            <p><b>4.8.23 - Lowell, MA </b>
+            <p><b>4.8.23 - Lowell, MA </b><a href='https://millno5.com/event/sons-lunaris-rumboat-chili-we-broke-the-weather/' target='_blank' rel="noopener noreferrer"> <FaInfoCircle/></a><br/>
             The Overlook<br/>  
             <small><a href='https://www.sonslunaris.com/' target='_blank' rel="noopener noreferrer">Sons Lunaris</a>, <a href='https://www.facebook.com/RumboatChili/' target='_blank' rel="noopener noreferrer">Rumboat Chili</a></small></p>
             
