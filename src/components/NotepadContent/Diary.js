bbc1028f4274a9735a3b100dc1c3c3318521cde2
kduckets <kmditroia@gmail.com>
2023-04-18 16:35:25 -0400

    Update Diary.js

--- src/components/NotepadContent/Diary.js
+++ src/components/NotepadContent/Diary.js
@@ -4,17 +4,90 @@ import React from 'react'
 function Diary({ content }) {
 
     return (
-        <div>
-            <h4>is prog cool again?<br/><small>by: kev</small></h4>
-                
-           <p>
         
-            <small>
-           [redacted]          </small>   </p>
-              <br/>
-              <br/>
+<div>
+<img alt="" src="https://mcusercontent.com/65c5bfbaf87d036d0dd3dcacc/images/5bbf2533-1511-6c36-7dd2-c699eed83bf8.png" style="max-width: 196px;padding-bottom: 0;display: inline !important;vertical-align: bottom;border: 0;height: auto;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;" class="mcnImage" width="196" align="middle"/>    
+<br/>        
+<h3>the weather report</h3>
+                
+<p>
+Hellooo weatherheads!!! Welcome to the inaugural w e a t h e r  r e p o r t, a friendly little newsletter for keeping our buds and pals up to date on all things we broke the weather.
+<br/>
 
+2022 was an exciting year for us. We released our debut album to critical acclaim (which we’re still floored by tbh), released a live-in-the-studio EP (along with some sick headless kev vids), and even got nominated for Boston Music Awards’ Jazz Artist of the Year–which is pretty neat. Best of all though, we got to stay up late and play some rock n roll for you, our fans and buds and pals. Words cannot describe our appreciation for y’all showing up for our string of late nite weeknight dive bar sets. It really means the world to us.
 
+We’re moving on up in 2023, comin at ya with weekend shows at potentially more reasonable hours. Next up:
+<br/>
+<img data-file-id="82238" src="https://mcusercontent.com/65c5bfbaf87d036d0dd3dcacc/images/1d17a463-decd-421f-63e4-4952eb45ada9.png" style="border: 0px;width: 271px;height: 420px;margin: 0px;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;" width="271" height="420"/>
+<a href="https://lunalowell.com/movies/sons-lunaris-rumboat-chili-we-broke-the-weather-4-8-630/" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #007C89;font-weight: normal;text-decoration: underline;">4/8 with Sons Lunaris and Rumboat Chili @ The Overlook in Lowell, MA</a>
+<br/>
+<br/>
+<a href="https://eventbrite.com/e/free-rock-the-cosmic-vultures-renee-maskin-tickets-593430676127" target="_blank" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #007C89;font-weight: normal;text-decoration: underline;">5/6 with Free Rock, Cosmic Vultures, and Renee Maskin&nbsp;@ The Jungle in Somerville MA</a>
+<br/>
+With more to come. We'll keep you posted here and at webroketheweather.com as shows crop up. <br/>
+Sadly, due to a scheduling conflict, we will not be playing Somerville Porchfest this year :(
+<br/>
+But rest assured, while we aren't on stage actively trying to decapitate you with sound, we’re also hard at work on new material, upgrading the practice space, cooking up demos, messin around with synths, revamping the live setup, etc etc 
+ <br/>
+- Steve</p>
+<br/>
+
+<hr class="rounded"></hr> 
+
+<h3>feast your earholes</h3>
+<h4>Nick's Picks</h4>
+<br/>
+<img data-file-id="82240" src="https://mcusercontent.com/65c5bfbaf87d036d0dd3dcacc/images/7af92216-5442-4616-ea60-ca815c0dc30b.jpg" style="border: 0px;width: 350px;height: 350px;margin: 0px;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;" width="350" height="350"></img>
+<br/>
+<p>
+<a href="https://aimingforenrike.bandcamp.com/album/empty-airports" target="_blank" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #007C89;font-weight: normal;text-decoration: underline;">Aiming For Enrike - Empty Airports</a> <b>(electronic, minimalism, math rock)</b>
+<br/>
+  The first time I saw Norway’s Aiming For Enrike was opening for fellow Norwegian jazz-rock juggernauts Jaga Jazzist in I think 2016 or ‘17. Back then the duo were more solidly in the playful/driving instrumental math rock space – think Battles, And So I Watch You From Afar, VASA, etc. I’ve loosely kept tabs on them since, but Empty Airports has caught my attention in a way they hadn’t before. Something about the mixture of smooth synths on top of their more typical guitar loops and themes just does something to me. Feels like a synthwave-y, cyberpunk-y beach party at sunset. Basically the vibes are immaculate.      
+</p>    
+<img data-file-id="82241" src="https://mcusercontent.com/65c5bfbaf87d036d0dd3dcacc/images/7c9fd555-42c3-49fc-0626-071e08768372.jpg" style="border: 0px;width: 350px;height: 350px;margin: 0px;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;" width="350" height="350"></img>
+<br/>
+<a href="https://atsukochiba.bandcamp.com/album/water-it-feels-like-its-growing" target="_blank" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #007C89;font-weight: normal;text-decoration: underline;">Atsuko Chiba - Water, It Feels Like It’s Growing</a> <b>(prog psych rock)</b>   
+<p>
+Montreal’s Atsuko Chiba have made a point of being near impossible to define, but whatever they’ve thrown out there has been both fascinating and an utter joy to listen to. Their winning streak continues on their latest, which I can basically describe as the psychedelic grooviness of Gizzard but just the more experimental parts and also after having taken an edible. Once again, vibes up the wazoo. Take me up to space and let me float amongst the stars to this shit.
+</p>
+<img data-file-id="82243" src="https://mcusercontent.com/65c5bfbaf87d036d0dd3dcacc/images/9c4eb1fb-f7f4-89f4-d1a5-a6268cbfee84.jpg" style="border: 0px;width: 350px;height: 350px;margin: 0px;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;" width="350" height="350"></img>
+<br/>
+<a href="https://witchripper.bandcamp.com/album/the-flight-after-the-fall-2" target="_blank" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #007C89;font-weight: normal;text-decoration: underline;">Witch Ripper - The Flight After the Fall</a> <b>(prog stoner metal)</b>
+ <p>
+ One of the qualities that pretty much without fail will make me love a piece of music is if I think to myself, “This shit sounds like Mastodon’s Blood Mountain!” If that doesn’t automatically conjure up a very specific image to you, then basically imagine music with big-ass riffs, big-ass melodies, and big-ass fuzz that straddles the line between being nerd shit and music to slam beer cans on your forehead to. There’s definitely some touches of more Mars Volta, Rush, The Dear Hunter, what-have-you kind of capital P prog as well if you’re into that (and if you’re reading this, I’m going on a hunch that you probably are).
+ </p>    
+
+ <h4>Steve's sleeve</h4>
+ <img data-file-id="82239" src="https://mcusercontent.com/65c5bfbaf87d036d0dd3dcacc/images/34249729-91a1-bf52-6534-dbf28e744eef.jpg" style="border: 0px;width: 350px;height: 350px;margin: 0px;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;" width="350" height="350"></img>
+ <br/>
+ <a href="https://delugegrander.bandcamp.com/album/oceanarium-2" target="_blank" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #007C89;font-weight: normal;text-decoration: underline;">Deluge Grander - Oceanarium</a> <b>(instrumental prog)</b>
+<p>
+Slip into your wizard robes, pack yourself a bowl of halflings leaf, and whisper some forbidden incantations because this is a straight up instrumental prog banger. This 2017 offering from Dan Britton’s Deluge Grander feels very much like an extension of his work in Birds and Buildings—which I typically describe as early King Crimson crossed with fusion era Zappa—only with more over the top instrumentation and somehow even more thematic composition. If you’re looking to score your next D&D campaign and/or just looking to escape to the shadow realm for an hour and change, look no further. 
+</p>
+<br/>
+<hr class="rounded"></hr> 
+
+<h3>and now, this</h3>
+<br/>
+<img data-file-id="82248" src="https://mcusercontent.com/65c5bfbaf87d036d0dd3dcacc/images/b2cdcf9a-5bd3-625f-c078-0524bc32a08d.jpg" style="border: 0px;width: 350px;height: 350px;margin: 0px;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;" width="350" height="350"></img>
+<br/>
+<h4>art, ai, baseball</h4>
+<p>
+Remember Moneyball? Does the name ‘Brad Pitt’ ring a bell? For those who don’t follow America’s pastime or watch B Pitt movies, the game of baseball fell victim to data and algorithms long before your new chatbot friend arrived into this mess of a reality. As the story goes, Brad and Jonah’s ideas led to more analytics-based strategies, and the blueprints of baseball were handed over to our ruthless robot overlords. For the first time ever, there was an unforeseen consequence of technology. (@Andy pls fact check this)
+<br/>
+<br/>
+Many argue that the robots made an already slow-paced game more predictable, uninspired, and some (not me) even say - boring. It certainly didn’t make it easier for an old, slow game to compete with instagram for our beloved humanity’s attention. Thanks, Brad.
+<br/>
+<br/>
+I’m not saying the new rules of baseball are going to cure your TikTok addiction. You’re gonna need much harder drugs for that. I’m suggesting that the most interesting ideas behind the rule changes are the attempts to dampen the soulless reverberations AI has injected into the game. We’re witnessing a human effort to make baseball more exciting, interesting, and weird. A slight shift to shake things up.
+<br/>
+<br/>
+If you didn’t like baseball before, you most likely still won’t. If you like baseball, you probably have a lot to say about all this. If you like chatting with bots and gazing longingly into the Eye of AI art, cool. At any rate, I think the lessons baseball offers us are at least worth thinking about. This one sure seems timely.
+<br/>
+<br/>
+Love,<br/>
+Kev 
+</p>
         </div>
 
         
