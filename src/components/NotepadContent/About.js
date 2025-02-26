3940239eb5d1bc62e4da992343759163ad1d0757
kduckets <kmditroia@gmail.com>
2025-02-26 15:39:21 -0500

    Update About.js

--- src/components/NotepadContent/About.js
+++ src/components/NotepadContent/About.js
@@ -92,14 +92,15 @@ function About({ content }) {
             </StyledDiv>
             
             
-            {/* <hr class="rounded"></hr> 
+          {/* <hr class="rounded"></hr> 
             <h3>next show:</h3>
             
             <p><b>2.8.25 - Lowell, MA</b><br/>Taffeta Music Hall<br/></p>
            <p>w/ <small><a href='https://clamb.bandcamp.com/' target='_blank' rel='noopener noreferrer'>Clamb</a> & <a href='https://therupertselection.bandcamp.com/' target='_blank' rel='noopener noreferrer'>The Rupert Selection</a></small></p>
            <a href='https://www.tixr.com/groups/taffetamusic/events/the-rubert-selection-clamb-we-broke-the-weather-125427' target='_blank' rel="noopener noreferrer">
             <img alt='wbtw-taffeta-25' src='/wbtw-clamb-rupert.png' style={{width:'100%'}}/>
-            </a>
+            </a> */}
+
             <hr class="rounded"></hr> 
             <h3>Restart Game OUT NOW!</h3>
          
@@ -107,7 +108,7 @@ function About({ content }) {
             <img src='/restart-game.jpg' alt='restart-game-cover' width='100%' ></img>
             </a> 
             <iframe width="100%" height="52" src="https://odesli.co/embed/?url=https%3A%2F%2Falbum.link%2Fkffgs2crmqdq3&theme=light" frameborder="0" allowfullscreen sandbox="allow-same-origin allow-scripts allow-presentation allow-popups allow-popups-to-escape-sandbox" allow="clipboard-read; clipboard-write"></iframe>            
-            */}
+        
      
            
 
