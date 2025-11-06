bca672454007b682fde5b28dd22a12ea51f959ab
kduckets <kmditroia@gmail.com>
2025-11-06 14:06:57 -0500

    content update

--- src/components/Notepad.js
+++ src/components/Notepad.js
@@ -1,40 +1,124 @@
-import React from 'react'
-import { Modal, Frame } from '@react95/core'
-import ContentFactory from './NotepadContent/ContentFactory';
+"use client"
+
+import { Modal, Frame } from "@react95/core"
+import styled from "styled-components"
+import { useRef, useEffect } from "react"
+import ContentFactory from "./NotepadContent/ContentFactory"
+
+// Icon component to load icons from CDN
+const IconSprite = ({ name }) => (
+  <i
+    style={{
+      display: "inline-block",
+      width: "16px",
+      height: "16px",
+      backgroundImage: `url(https://raw.githubusercontent.com/React95/React95/master/packages/icons/src/icons/${name}.ico)`,
+      backgroundSize: "contain",
+      backgroundRepeat: "no-repeat",
+      verticalAlign: "middle",
+    }}
+  />
+)
+
+const ContentWrapper = styled.div`
+    padding: 10px 20px 20px 20px;
+    
+    /* Center all headings */
+    h1, h2, h3, h4, h5, h6 {
+        text-align: center;
+        margin: 10px 0 15px 0;
+    }
+    
+    /* Remove top margin from first child element */
+    > *:first-child {
+        margin-top: 0;
+    }
+    
+    /* Center paragraphs and add spacing */
+    p {
+        text-align: center;
+        margin: 15px 0;
+        line-height: 1.6;
+    }
+    
+    /* Center images */
+    img {
+        display: block;
+        margin: 20px auto;
+        max-width: 100%;
+        height: auto;
+    }
+    
+    /* Center divs and add spacing */
+    > div {
+        text-align: center;
+        margin: 15px 0;
+    }
+    
+    /* Style links */
+    a {
+        color: #0000ff;
+        text-decoration: underline;
+        cursor: pointer;
+        
+        &:visited {
+            color: #800080;
+        }
+        
+        &:hover {
+            color: #ff0000;
+        }
+    }
+    
+    /* Add spacing between sections */
+    hr {
+        margin: 30px 0;
+        border: none;
+        border-top: 1px solid #808080;
+    }
+`
 
 function Notepad({ closeNotepad, selectedItem, isMobile }) {
-    return (
-        <Modal
-            icon="notepad"
-            title={`${selectedItem.name}`}
-            closeModal={closeNotepad}
-            buttons={[{ value: "Close", onClick: closeNotepad }]}
-            style={{
-                left: isMobile ? '5%' : '32%',
-                top: isMobile ? '3%' : '5%',
-                width: isMobile ? '90%' : 450
-            }}
-            menu={[
-                { name: 'File', list: [] },
-                { name: 'Edit', list: [] }
-            ]}>
-            <Frame
-                bg="white"
-                boxShadow="in"
-                height="100%"
-                paddingLeft={20}
-                paddingRight={20}
-                paddingBottom={20}
-                paddingTop={4}
-                style={{
-					overflowY: "auto",
-					maxHeight: isMobile ? '60vh' : '72vh'
-				}}
-            >
-            <ContentFactory id={selectedItem.id} isMobile={isMobile} />
-            </Frame>
-        </Modal >
-    )
+  const scrollRef = useRef(null)
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = 0
+    }
+  }, [selectedItem.id])
+
+  return (
+    <Modal
+      icon={<IconSprite name="notepad" />}
+      title={`${selectedItem.name}`}
+      closeModal={closeNotepad}
+      buttons={[{ value: "Close", onClick: closeNotepad }]}
+      style={{
+        left: isMobile ? "5%" : "32%",
+        top: isMobile ? "3%" : "5%",
+        width: isMobile ? "90%" : 450,
+      }}
+      menu={[
+        { name: "File", list: [] },
+        { name: "Edit", list: [] },
+      ]}
+    >
+      <Frame
+        bg="white"
+        boxShadow="in"
+        height="100%"
+        ref={scrollRef}
+        style={{
+          overflowY: "auto",
+          maxHeight: isMobile ? "60vh" : "72vh",
+        }}
+      >
+        <ContentWrapper>
+          <ContentFactory id={selectedItem.id} isMobile={isMobile} />
+        </ContentWrapper>
+      </Frame>
+    </Modal>
+  )
 }
 
 export default Notepad
--- src/components/NotepadContent/Merch.js
+++ src/components/NotepadContent/Merch.js
@@ -1,8 +1,7 @@
-import React from 'react'
-import styled from 'styled-components'
-import { FaInstagram, FaFacebook, FaBandcamp, FaEnvelope } from 'react-icons/fa'
+import styled from "styled-components"
+import { FaInstagram, FaFacebook, FaBandcamp, FaEnvelope } from "react-icons/fa"
 
-const Icons = { FaInstagram, FaFacebook, FaBandcamp, FaEnvelope };
+const Icons = { FaInstagram, FaFacebook, FaBandcamp, FaEnvelope }
 
 const StyledItem = styled.li`
     display: inline;
@@ -22,97 +21,205 @@ const StyledDiv = styled.div`
     text-align: center;
     margin: auto;
     width: 100%;
-    `
+    padding: 20px 0;
+`
+
+const ProductSection = styled.div`
+    text-align: center;
+    margin: 30px 0;
+    padding: 20px 0;
+`
+
+const ProductTitle = styled.h3`
+    margin: 20px 0 10px 0;
+    font-weight: bold;
+`
+
+const ProductDescription = styled.p`
+    margin: 10px 0;
+    line-height: 1.5;
+`
+
+const ProductImage = styled.img`
+    display: block;
+    margin: 20px auto;
+    max-width: 90%;
+    height: auto;
+`
+
+const ProductPrice = styled.h4`
+    margin: 15px 0;
+    font-size: 18px;
+    font-weight: bold;
+`
+
+const ProductButton = styled.button`
+    padding: 8px 16px;
+    background-color: #c0c0c0;
+    border: 2px solid;
+    border-color: #ffffff #808080 #808080 #ffffff;
+    cursor: pointer;
+    font-family: inherit;
+    font-size: 14px;
+    
+    &:active {
+        border-color: #808080 #ffffff #ffffff #808080;
+    }
+`
+
+const Divider = styled.hr`
+    margin: 30px 0;
+    border: 0;
+    border-top: 1px solid #808080;
+`
 
 function Merch({ content }) {
   const social = [
-
     {
-      name: 'FaBandcamp',
-      link: 'https://webroketheweather.bandcamp.com?campaign=website',
+      name: "FaBandcamp",
+      link: "https://webroketheweather.bandcamp.com?campaign=website",
     },
     {
-      name: 'FaInstagram',
-      link: 'https://www.instagram.com/webroketheweather/',
+      name: "FaInstagram",
+      link: "https://www.instagram.com/webroketheweather/",
     },
     {
-      name: 'FaFacebook',
-      link: 'https://facebook.com/webroketheweather',
+      name: "FaFacebook",
+      link: "https://facebook.com/webroketheweather",
     },
     {
-      name: 'FaEnvelope',
-      link: 'mailto:webroketheweather@gmail.com',
+      name: "FaEnvelope",
+      link: "mailto:webroketheweather@gmail.com",
     },
-
   ]
 
-    return (
-        <div>
-          <StyledDiv>
-            <h2>we broke the weather</h2>
-            <StyledList>
-            {
-                    social.map((s, idx) => {
-                        const Icon = Icons[s.name];
-                        return (
-                            <StyledItem key={idx}>
-                                <StyledLink href={s.link} target="_blank" rel="noopener noreferrer">
-                                    <Icon />
-                                </StyledLink>
-                            </StyledItem>
-                        );
-                    })
-                }
-                </StyledList>
-                </StyledDiv>
+  return (
+    <div>
+      <StyledDiv>
+        <h2>we broke the weather</h2>
+        <StyledList>
+          {social.map((s, idx) => {
+            const Icon = Icons[s.name]
+            return (
+              <StyledItem key={idx}>
+                <StyledLink href={s.link} target="_blank" rel="noopener noreferrer">
+                  <Icon />
+                </StyledLink>
+              </StyledItem>
+            )
+          })}
+        </StyledList>
+      </StyledDiv>
 
-            <hr class="rounded"></hr>
-            <h3>eco wallet compact disc</h3>  
-            <p>CD comes in 100% plastic-free eco wallet gatefold</p>
-           
-            <a href="https://webroketheweather.bandcamp.com/album/we-broke-the-weather?campaign=website" target="_blank" rel="noopener noreferrer">
-            <img src='/cd.jpg' alt='wbtw-cd' width='90%' ></img>
-            </a>
-            <h4>$10</h4>
-            <p>
-            <a href="https://webroketheweather.bandcamp.com/album/we-broke-the-weather?campaign=website" target="_blank" rel="noopener noreferrer"><button>order on bandcamp</button></a>
-            </p>
-          <br/>
-          <hr class="rounded"></hr> 
-      
-          <h3>t-shirts</h3>
-          <p>wish your friends said “hey, cool tshirt” more often? try this one.</p>  
-          <a href="https://webroketheweather.bandcamp.com/merch/rot-king-t-shirt?campaign=website" target="_blank" rel="noopener noreferrer">
-            <img src='/t-shirt.jpg' alt='wbtw-tshirt' width='90%' ></img>
-            </a>
-            <h4>$20</h4>
-            <p>
-            <a href="https://webroketheweather.bandcamp.com/merch/rot-king-t-shirt?campaign=website" target="_blank" rel="noopener noreferrer"><button>order on bandcamp</button></a>
-            </p>
-          <br/>
-          <br/>
-          <hr class="rounded"></hr> 
-            <h3>sticker pack</h3>
-          <p>grab some stickers for your stickerless surfaces</p>  
-          <a href="https://webroketheweather.bandcamp.com/merch/wbtw-vinyl-sticker-pack?campaign=website" target="_blank" rel="noopener noreferrer">
-            <img src='/fire sticker.jpg' alt='wbtw-fire-sticker' width='90%' ></img>
-            </a>
-            <h4>$5</h4>
-            <p>
-            <a href="https://webroketheweather.bandcamp.com/merch/wbtw-vinyl-sticker-pack?campaign=website" target="_blank" rel="noopener noreferrer"><button>order on bandcamp</button></a>
-            </p>
-          <br/>
-          <br/>
-          <hr class="rounded"></hr> 
-            <br/>
-          
-            <div>
+      <Divider />
 
-              <br/>
-           </div>  
+      <ProductSection>
+        <ProductTitle>restart game vinyl</ProductTitle>
+        <ProductDescription>12" vinyl record - limited edition pressing (ox blood)</ProductDescription>
 
-        </div>
-    )
+        <a href="https://webroketheweather.bandcamp.com/album/restart-game" target="_blank" rel="noopener noreferrer">
+          <ProductImage src="/vinyl.jpg" alt="restart-game-vinyl" />
+        </a>
+        <ProductPrice>$25</ProductPrice>
+        <p>
+          <a href="https://webroketheweather.bandcamp.com/album/restart-game" target="_blank" rel="noopener noreferrer">
+            <ProductButton>order on bandcamp</ProductButton>
+          </a>
+        </p>
+      </ProductSection>
+
+      <Divider />
+
+      <ProductSection>
+        <ProductTitle>black hole cactus t-shirt</ProductTitle>
+        <ProductDescription>premium comfort tee with a black hole and a cactus</ProductDescription>
+        <a
+          href="https://webroketheweather.bandcamp.com/merch/black-hole-cactus-t-shirt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <ProductImage src="/cactus_tee.jpg" alt="black-hole-cactus-tshirt" />
+        </a>
+        <ProductPrice>$20</ProductPrice>
+        <p>
+          <a
+            href="https://webroketheweather.bandcamp.com/merch/black-hole-cactus-t-shirt"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <ProductButton>order on bandcamp</ProductButton>
+          </a>
+        </p>
+      </ProductSection>
+
+      <Divider />
+
+      <ProductSection>
+        <ProductTitle>restart game cd</ProductTitle>
+        <ProductDescription>CD comes in 100% plastic-free eco wallet gatefold</ProductDescription>
+
+        <a href="https://webroketheweather.bandcamp.com/album/restart-game" target="_blank" rel="noopener noreferrer">
+          <ProductImage src="/rg-cd.jpg" alt="restart-game-cd" />
+        </a>
+        <ProductPrice>$10</ProductPrice>
+        <p>
+          <a href="https://webroketheweather.bandcamp.com/album/restart-game" target="_blank" rel="noopener noreferrer">
+            <ProductButton>order on bandcamp</ProductButton>
+          </a>
+        </p>
+      </ProductSection>
+
+      <Divider />
+
+      <ProductSection>
+        <ProductTitle>rot king t-shirt</ProductTitle>
+        <ProductDescription>wish your friends said "hey, cool tshirt" more often? try this one.</ProductDescription>
+        <a
+          href="https://webroketheweather.bandcamp.com/merch/rot-king-t-shirt?campaign=website"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <ProductImage src="/t-shirt.jpg" alt="wbtw-tshirt" />
+        </a>
+        <ProductPrice>$20</ProductPrice>
+        <p>
+          <a
+            href="https://webroketheweather.bandcamp.com/merch/rot-king-t-shirt?campaign=website"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <ProductButton>order on bandcamp</ProductButton>
+          </a>
+        </p>
+      </ProductSection>
+
+      <Divider />
+
+      <ProductSection>
+        <ProductTitle>sticker pack</ProductTitle>
+        <ProductDescription>grab some stickers for your stickerless surfaces</ProductDescription>
+        <a
+          href="https://webroketheweather.bandcamp.com/merch/wbtw-vinyl-sticker-pack?campaign=website"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <ProductImage src="/fire sticker.jpg" alt="wbtw-fire-sticker" />
+        </a>
+        <ProductPrice>$5</ProductPrice>
+        <p>
+          <a
+            href="https://webroketheweather.bandcamp.com/merch/wbtw-vinyl-sticker-pack?campaign=website"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <ProductButton>order on bandcamp</ProductButton>
+          </a>
+        </p>
+      </ProductSection>
+
+      <Divider />
+    </div>
+  )
 }
 
 export default Merch
