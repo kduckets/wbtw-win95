f6788a0e663e4f3a70bc45de9fbdff97ac47bbb4
kduckets <kmditroia@gmail.com>
2025-11-06 14:17:39 -0500

    icons

--- src/components/Explorer.js
+++ src/components/Explorer.js
@@ -1,50 +1,53 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Modal, Frame } from '@react95/core'
-import Item from './Item'
+import styled from "styled-components"
+import { Modal, Frame } from "@react95/core"
+import Item from "./Item"
 
 const FilesWrapper = styled.div`
-	display: flex;
-	grid-template-columns: repeat(4, 1fr);
-	flex-wrap: wrap;
-`;
+  display: flex;
+  grid-template-columns: repeat(4, 1fr);
+  flex-wrap: wrap;
+`
 
+const IconSprite = ({ name }) => (
+  <i
+    style={{
+      display: "inline-block",
+      width: "16px",
+      height: "16px",
+      backgroundImage: `url(https://raw.githubusercontent.com/React95/React95/master/packages/icons/src/icons/${name}.ico)`,
+      backgroundSize: "contain",
+      backgroundRepeat: "no-repeat",
+      verticalAlign: "middle",
+    }}
+  />
+)
 
 function Explorer({ items, closeExplorer, openNotepad, isMobile }) {
-    return (
-        <Modal
-            icon="explorer_100"
-            title="my computer"
-            closeModal={closeExplorer}
-            style={{
-                left: isMobile ? '5%' : '15%',
-                top: '30%',
-                width: isMobile ? '90%' : 400,
-            }}
-            menu={[
-                { name: 'File', list: [] },
-                { name: 'Edit', list: [] },
-                { name: 'Help', list: [] },
-            ]}>
-            <Frame
-                bg="white"
-                boxShadow="in"
-                height="100%"
-            >
-                <FilesWrapper>
-                    {
-                        items.map((item) => (
-                            <Item
-                                key={item.id}
-                                item={item}
-                                openNotepad={openNotepad}
-                            />
-                        ))
-                    }
-                </FilesWrapper>
-            </Frame>
-        </Modal>
-    )
+  return (
+    <Modal
+      icon={<IconSprite name="folder_file" />}
+      title="Documents"
+      closeModal={closeExplorer}
+      buttons={[{ value: "Close", onClick: closeExplorer }]}
+      style={{
+        left: isMobile ? "5%" : "15%",
+        top: isMobile ? "15%" : "10%",
+        width: isMobile ? "90%" : 500,
+      }}
+      menu={[
+        { name: "File", list: [] },
+        { name: "Edit", list: [] },
+      ]}
+    >
+      <Frame bg="white" boxShadow="in" height="100%" padding={20}>
+        <FilesWrapper>
+          {items.map((item) => (
+            <Item key={item.id} item={item} openNotepad={openNotepad} />
+          ))}
+        </FilesWrapper>
+      </Frame>
+    </Modal>
+  )
 }
 
 export default Explorer
--- src/components/Synth/Synth.js
+++ src/components/Synth/Synth.js
@@ -1,61 +1,60 @@
-import React, { useState } from "react";
-import { Modal } from '@react95/core';
-import Oscillator from "./Oscillator";
-import ChangeWaveform from "./Waveform";
-import Gain from "./Gain";
-import Filter from "./Filter";
+"use client"
+
+import { useState } from "react"
+import { Modal } from "@react95/core"
+import Oscillator from "./Oscillator"
+import ChangeWaveform from "./Waveform"
+import Gain from "./Gain"
+import Filter from "./Filter"
 import "./piano.css"
 
+const IconSprite = ({ name }) => (
+  <i
+    style={{
+      display: "inline-block",
+      width: "16px",
+      height: "16px",
+      backgroundImage: `url(https://raw.githubusercontent.com/React95/React95/master/packages/icons/src/icons/${name}.ico)`,
+      backgroundSize: "contain",
+      backgroundRepeat: "no-repeat",
+      verticalAlign: "middle",
+    }}
+  />
+)
 
-function Synth ({ closeSynth, isMobile }) {
+function Synth({ closeSynth, isMobile }) {
   const [effects, setEffects] = useState({
     waveform: "sine",
     gainValue: 0.15,
     filterType: "lowpass",
-    filterFreq: 15000
-  });
+    filterFreq: 15000,
+  })
 
-  const inputChange = e => {
-    setEffects({ ...effects, [e.target.name]: e.target.value });
-  };
+  const inputChange = (e) => {
+    setEffects({ ...effects, [e.target.name]: e.target.value })
+  }
 
   return (
     <Modal
-    icon="mmsys_90"
-    title="synth.exe"
-    closeModal={closeSynth}
-    style={{
-        left: isMobile ? '5%' : '5%',
-        top: '5%',
-        width: isMobile ? '90%' : '80%',
-        height: isMobile ? '86%' : '90%'
-    }}
-    buttons={[
-
-      { value: 'Exit', onClick: closeSynth },
-
-      ]}>
-
-    <div className="piano">
-        <Oscillator
-          waveform={effects.waveform}
-          filterType={effects.filterType}
-          filterFreq={effects.filterFreq}
-          gainValue={effects.gainValue}
-        />
-      </div>
-      <div className="effects">
-        <ChangeWaveform inputChange={inputChange} waveform={effects.waveform} />
-        <Gain inputChange={inputChange} gainValue={effects.gainValue} />
-        <Filter
-          inputChange={inputChange}
-          filterType={effects.filterType}
-          filterFreq={effects.filterFreq}
-        />
-      </div>
-
+      icon={<IconSprite name="media_cd_5" />}
+      title="Control Panel"
+      closeModal={closeSynth}
+      style={{
+        left: isMobile ? "5%" : "25%",
+        top: isMobile ? "10%" : "5%",
+        width: isMobile ? "90%" : 700,
+      }}
+      menu={[
+        { name: "File", list: [] },
+        { name: "Edit", list: [] },
+      ]}
+    >
+      <ChangeWaveform inputChange={inputChange} />
+      <Gain inputChange={inputChange} />
+      <Filter inputChange={inputChange} />
+      <Oscillator effects={effects} />
     </Modal>
-  );
-};
+  )
+}
 
 export default Synth
