7fc8b1681c3e8cdf0e48997c47e94d2e3088060b
kduckets <kmditroia@gmail.com>
2025-11-06 13:43:46 -0500

    icon spacing

--- src/components/Taskbar.js
+++ src/components/Taskbar.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { TaskBar, List } from '@react95/core'
 import { NavLink } from "react-router-dom"
+import './Taskbar.css'
 
-// Import icons as images from a CDN or use inline styles
 const IconSprite = ({ name }) => (
     <i 
-        className={`icon ${name}`}
         style={{
             display: 'inline-block',
             width: '16px',
@@ -13,70 +12,72 @@ const IconSprite = ({ name }) => (
             backgroundImage: `url(https://raw.githubusercontent.com/React95/React95/master/packages/icons/src/icons/${name}.ico)`,
             backgroundSize: 'contain',
             backgroundRepeat: 'no-repeat',
-            marginRight: '4px'
+            verticalAlign: 'middle'
         }}
     />
 )
 
 function Taskbar({openExplorer, openPaint, openMine, openFloppy, openSpace, openSynth, openNotepad}) {
     return (
-        <TaskBar
-            list={
-                <List>
-                    <List.Item icon={<IconSprite name="folder_exe" />}>
-                        <List>
-                            <List.Item icon={<IconSprite name="mspaint" />} onClick={() => openPaint()}>
-                                Paint
-                            </List.Item>
-                            
-                            <List.Item icon={<IconSprite name="winmine_1" />} onClick={() => openMine()}>
-                                Minesweeper
-                            </List.Item>
-                            
-                            <List.Item icon={<IconSprite name="joy_102" />} onClick={() => openFloppy()}>
-                                Floppy Box
-                            </List.Item>
+        <div className="taskbar-list">
+            <TaskBar
+                list={
+                    <List>
+                        <List.Item icon={<IconSprite name="folder_exe" />}>
+                            <List>
+                                <List.Item icon={<IconSprite name="mspaint" />} onClick={() => openPaint()}>
+                                    Paint
+                                </List.Item>
+                                
+                                <List.Item icon={<IconSprite name="winmine_1" />} onClick={() => openMine()}>
+                                    Minesweeper
+                                </List.Item>
+                                
+                                <List.Item icon={<IconSprite name="joy_102" />} onClick={() => openFloppy()}>
+                                    Floppy Box
+                                </List.Item>
 
-                            <List.Item icon={<IconSprite name="flying_through_space_100" />} onClick={() => openSpace()}>
-                                Space Invaders
-                            </List.Item>
-                        </List>
-                        Programs
-                    </List.Item>
-                    
-                    <List.Item icon={<IconSprite name="folder_file" />} onClick={() => openExplorer()}>
-                        Documents
-                    </List.Item>
-                    
-                    <List.Item icon={<IconSprite name="settings" />}>
-                        <List>
-                            <List.Item icon={<IconSprite name="folder_settings" />} onClick={() => openSynth()}>
-                                Control Panel
-                            </List.Item>
-                        </List>
-                        Settings
-                    </List.Item>
-                    
-                    <List.Item icon={<IconSprite name="help_book" />} onClick={() => openNotepad({
-                        id: 'credits',
-                        name: 'credits',
-                        icon: 'mmsys_90',
-                    })}>
-                        Help
-                    </List.Item>
-                    
-                    <List.Item icon={<IconSprite name="loader_bat" />}>Run...</List.Item>
-                    
-                    <List.Divider />
-                    
-                    <List.Item icon={<IconSprite name="computer_3" />}>
-                        <NavLink to="/404" style={{color:'inherit', textDecoration:'inherit'}}>
-                            Shut Down...
-                        </NavLink>
-                    </List.Item>
-                </List>
-            }
-        />
+                                <List.Item icon={<IconSprite name="flying_through_space_100" />} onClick={() => openSpace()}>
+                                    Space Invaders
+                                </List.Item>
+                            </List>
+                            Programs
+                        </List.Item>
+                        
+                        <List.Item icon={<IconSprite name="folder_file" />} onClick={() => openExplorer()}>
+                            Documents
+                        </List.Item>
+                        
+                        <List.Item icon={<IconSprite name="settings" />}>
+                            <List>
+                                <List.Item icon={<IconSprite name="folder_settings" />} onClick={() => openSynth()}>
+                                    Control Panel
+                                </List.Item>
+                            </List>
+                            Settings
+                        </List.Item>
+                        
+                        <List.Item icon={<IconSprite name="help_book" />} onClick={() => openNotepad({
+                            id: 'credits',
+                            name: 'credits',
+                            icon: 'mmsys_90',
+                        })}>
+                            Help
+                        </List.Item>
+                        
+                        <List.Item icon={<IconSprite name="loader_bat" />}>Run...</List.Item>
+                        
+                        <List.Divider />
+                        
+                        <List.Item icon={<IconSprite name="computer_3" />}>
+                            <NavLink to="/404" style={{color:'inherit', textDecoration:'inherit'}}>
+                                Shut Down...
+                            </NavLink>
+                        </List.Item>
+                    </List>
+                }
+            />
+        </div>
     )
 }
 
