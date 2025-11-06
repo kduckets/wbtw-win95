84d4699ea204db290fe929ffd3448eac8b6fba3c
kduckets <kmditroia@gmail.com>
2025-11-06 10:44:56 -0500

    icon

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,9 +1,6 @@
-// src/components/Icon.js
 import React from 'react';
 
-function Icon({ name, className, onClick }) {
-  // Since icon names have underscores and can't be imported directly,
-  // we'll render them as img elements using the CDN or local paths
+function Icon({ name, onClick, className }) {
   return (
     <img 
       src={`https://raw.githubusercontent.com/React95/React95/HEAD/packages/icons/src/icons/${name}.ico`}
