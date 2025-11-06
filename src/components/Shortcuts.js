8418b4b33166d93a86b23d7ca8505ce7a95dbb2f
kduckets <kmditroia@gmail.com>
2025-11-06 10:58:32 -0500

    Update Shortcuts.js

--- src/components/Shortcuts.js
+++ src/components/Shortcuts.js
@@ -1,36 +1,47 @@
 import React from 'react'
 import styled from 'styled-components'
-import Icon from './Icon'  // <CHANGE> Changed from named import to default import
+import Icon from './Icon'
 import {startWebamp} from '../utils/startWebamp';
 
+
 const StyledShorcut = styled.div`
-margin-left: 20px;
-margin-top: 20px;
-align-items: center !important;
-color: white !important;
+    margin-left: 20px;
+    margin-top: 20px;
+	align-items: center !important;
+    color: white !important;
 `;
 
 function Shortcuts({openExplorer, openMine, openPaint, openNotepad, openFloppy, openSpace, items }) {
-  return (
-    <StyledShorcut>
-      <Icon
-        name="media_cd"
-        onClick={() => startWebamp()}
-      />
-      music 
-      
-      {
-        items.map((item) => (
-          <Icon
-            key={item.name}
-            name="txt_icon"
-            onClick={() => openNotepad(item)}
-          />
-          {item.name.replace(".txt","") } 
-        ))
-      }
-    </StyledShorcut>
-  )
+
+    return (
+        <div>
+
+            <StyledShorcut>
+                <Icon
+                    className="pointer"
+                    name="media_cd"
+                    onClick={()=>startWebamp()}
+                />
+                <div>music</div>
+            </StyledShorcut> 
+     
+     
+            {
+            items.map((item) => (
+             <StyledShorcut key={item.id}>
+                    <Icon
+                className="pointer"
+                name={item.icon}
+                onClick={() => openNotepad(item)}
+                     />
+                    <div>{item.name.replace(".txt","") }</div>
+            </StyledShorcut>
+                ))
+            }    
+   
+       
+        </div>
+    )
 }
 
 export default Shortcuts
